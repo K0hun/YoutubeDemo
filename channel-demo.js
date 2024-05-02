13768a1ac61950c4a974238ec5b8454a5ac44619
Hun <dudgns9194@gmail.com>
2024-05-02 17:49:46 +0900

    채널 전체 조회

--- channel-demo.js
+++ channel-demo.js
@@ -12,6 +12,20 @@ db.set(id++, {'channelTitle' : 'Hi~channel'});
 app
     .route('/channels')
     .get((req, res) => {
+        if(db.size){
+            var channels = [];
+        
+            db.forEach((value, key)=>{
+                channels.push(value);
+            });
+    
+            res.status(200).json(channels);
+        }else{
+            res.status(404).json({
+                message: '조회할 채널이 없습니다.'
+            });
+        }
+        
         
     }) // 채널 전체 조회
     .post((req, res) => {
